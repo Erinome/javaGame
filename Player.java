e4af0f11d8c21fb481b17b2f87b9e6c1431bad25
erinome <rebanee@tlu.ee>
2020-04-12 14:21:26 +0300

    Esimene push

---
+++ Player.java
@@ -0,0 +1,20 @@
+import java.awt.*;
+
+public class Player extends GameObject {
+
+    // Random r = new Random();
+    Handler handler = new Handler();
+
+    public Player(int x, int y, ID id, Handler handler) {
+        super(x, y, id);
+
+    }
+
+    public void tick() {
+    }
+
+    public void render(Graphics g) {
+
+    }
+
+}
