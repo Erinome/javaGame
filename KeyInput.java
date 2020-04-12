86b45c470c93cfb92c321f9f48e4bfe3783156f0
erinome <rebanee@tlu.ee>
2020-04-12 15:50:33 +0300

    Neljas push

--- KeyInput.java
+++ KeyInput.java
@@ -20,16 +20,16 @@ public class KeyInput extends KeyAdapter {
 
                 if (key == KeyEvent.VK_W) tempObject.setVelY(-5);
                 if (key == KeyEvent.VK_S) tempObject.setVelY(5);
-                if (key == KeyEvent.VK_D) tempObject.setVelY(5);
-                if (key == KeyEvent.VK_A) tempObject.setVelY(-5);
+                if (key == KeyEvent.VK_D) tempObject.setVelX(5);
+                if (key == KeyEvent.VK_A) tempObject.setVelX(-5);
             }
               if(tempObject.getId() == ID.Player2){
                   //key events for player 2
 
                   if (key == KeyEvent.VK_UP) tempObject.setVelY(-5);
                   if (key == KeyEvent.VK_DOWN) tempObject.setVelY(5);
-                  if (key == KeyEvent.VK_RIGHT) tempObject.setVelY(5);
-                  if (key == KeyEvent.VK_LEFT) tempObject.setVelY(-5);
+                  if (key == KeyEvent.VK_RIGHT) tempObject.setVelX(5);
+                  if (key == KeyEvent.VK_LEFT) tempObject.setVelX(-5);
               }      
             }
         }
@@ -46,16 +46,16 @@ public class KeyInput extends KeyAdapter {
 
                 if (key == KeyEvent.VK_W) tempObject.setVelY(0);
                 if (key == KeyEvent.VK_S) tempObject.setVelY(0);
-                if (key == KeyEvent.VK_D) tempObject.setVelY(0);
-                if (key == KeyEvent.VK_A) tempObject.setVelY(0);
+                if (key == KeyEvent.VK_D) tempObject.setVelX(0);
+                if (key == KeyEvent.VK_A) tempObject.setVelX(0);
             }
               if(tempObject.getId() == ID.Player2){
                   //key events for player 2
 
                   if (key == KeyEvent.VK_UP) tempObject.setVelY(0);
                   if (key == KeyEvent.VK_DOWN) tempObject.setVelY(0);
-                  if (key == KeyEvent.VK_RIGHT) tempObject.setVelY(0);
-                  if (key == KeyEvent.VK_LEFT) tempObject.setVelY(0);
+                  if (key == KeyEvent.VK_RIGHT) tempObject.setVelX(0);
+                  if (key == KeyEvent.VK_LEFT) tempObject.setVelX(0);
               }      
             }
 
